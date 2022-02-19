8cbe8521f5f61be79d8f65db70b854e42ee419ff
Ryan Sullivan <ryanpnavillus@gmail.com>
2022-02-19 00:20:03 -0500

    Initial Commit

---
+++ Source/TilesAndTowers/TilesAndTowersGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TilesAndTowersGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TILESANDTOWERS_API ATilesAndTowersGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
